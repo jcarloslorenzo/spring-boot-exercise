7bae5f551919d6712249d698282bfa24dae779df
Jose Carlos Lorenzo <128424207+jcarloslorenzo@users.noreply.github.com>
2025-03-11 03:30:55 +0100

    test(currency-client): correcciones en los test unitarios de cliente de 'currency'

--- spring-boot-exercise-currency-client/src/test/java/es/jclorenzo/exercises/springboot/currency/test/CurrencyClientTest.java
+++ spring-boot-exercise-currency-client/src/test/java/es/jclorenzo/exercises/springboot/currency/test/CurrencyClientTest.java
@@ -1,21 +1,27 @@
 package es.jclorenzo.exercises.springboot.currency.test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClient.RequestHeadersSpec;
+import org.springframework.web.client.RestClient.RequestHeadersUriSpec;
+import org.springframework.web.client.RestClient.ResponseSpec;
 
-import es.jclorenzo.exercises.springboot.currency.client.CurrencyRemoteClient;
 import es.jclorenzo.exercises.springboot.currency.client.enums.CurrencyEndpoints;
-import es.jclorenzo.exercises.springboot.currency.config.ModuleConfiguration;
+import es.jclorenzo.exercises.springboot.currency.client.impl.CurrencyRemoteClientImpl;
+import es.jclorenzo.exercises.springboot.currency.exception.RemoteSystemAccessException;
 import es.jclorenzo.exercises.springboot.currency.model.Currency;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,18 +29,30 @@ import lombok.extern.slf4j.Slf4j;
  * The Class CurrencyClientTest.
  */
 @Slf4j
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ModuleConfiguration.class)
-@TestPropertySource(value = "classpath:application.properties")
+@ExtendWith(MockitoExtension.class)
 public class CurrencyClientTest {
 
 	/** The rest template. */
 	@Mock
-	private RestTemplate restTemplate;
+	private RestClient restClient;
+
+	/** The request headers uri spec. */
+	@Mock
+	@SuppressWarnings("rawtypes")
+	private RequestHeadersUriSpec requestHeadersUriSpec;
+
+	/** The request headers spec. */
+	@Mock
+	@SuppressWarnings("rawtypes")
+	private RequestHeadersSpec requestHeadersSpec;
+
+	/** The response spec. */
+	@Mock
+	private ResponseSpec responseSpec;
 
 	/** The user remote service. */
-	@Autowired
-	private CurrencyRemoteClient currencyClient;
+	@InjectMocks
+	private CurrencyRemoteClientImpl currencyClient;
 
 	/** The currencies. */
 	private final List<Currency> currencies = List.of(
@@ -45,19 +63,22 @@ public class CurrencyClientTest {
 	 * Find currencies test.
 	 */
 	@Test
+	@SuppressWarnings({ "unchecked" })
 	public void findCurrenciesTest() {
 
-		Mockito.when(
-				this.restTemplate.getForObject(
-						CurrencyEndpoints.CURRENCIES.getValue(),
-						List.class))
-				.thenReturn(this.currencies);
+		Mockito.when(this.restClient.get())
+				.thenReturn(this.requestHeadersUriSpec);
+		Mockito.when(this.requestHeadersUriSpec.uri(CurrencyEndpoints.CURRENCIES.getValue()))
+				.thenReturn(this.requestHeadersSpec);
+		Mockito.when(this.requestHeadersSpec.retrieve())
+				.thenReturn(this.responseSpec);
+		Mockito.when(this.responseSpec.body(new ParameterizedTypeReference<List<Currency>>() {
+		})).thenReturn(this.currencies);
 
 		Assertions.assertDoesNotThrow(() -> {
 			final List<Currency> result = this.currencyClient.getCurrencies();
 			Assertions.assertNotNull(result);
 			Assertions.assertFalse(result.isEmpty());
-			CurrencyClientTest.log.debug("Currencies -> {}", result);
 		});
 	}
 
@@ -65,21 +86,67 @@ public class CurrencyClientTest {
 	 * Find currency euro test.
 	 */
 	@Test
+	@SuppressWarnings({ "unchecked" })
 	public void findCurrencyEuroTest() {
 		final String currencyCode = "EUR";
 
-		Mockito.when(
-				this.restTemplate.getForObject(
-						CurrencyEndpoints.CURRENCY.getValue(),
-						List.class,
-						currencyCode))
-				.thenReturn(this.currencies);
+		Mockito.when(this.restClient.get())
+				.thenReturn(this.requestHeadersUriSpec);
+		Mockito.when(this.requestHeadersUriSpec.uri(CurrencyEndpoints.CURRENCY.getValue(), currencyCode))
+				.thenReturn(this.requestHeadersSpec);
+		Mockito.when(this.requestHeadersSpec.retrieve())
+				.thenReturn(this.responseSpec);
+		Mockito.when(this.responseSpec.body(Currency.class))
+				.thenReturn(this.currencies.get(0));
 
 		Assertions.assertDoesNotThrow(() -> {
 			final Currency result = this.currencyClient.getCurrencyByCode(currencyCode);
 			Assertions.assertNotNull(result);
 			Assertions.assertEquals(currencyCode, result.code());
-			CurrencyClientTest.log.debug("Currency -> {}", result);
+		});
+	}
+
+	/**
+	 * Not found error.
+	 */
+	@Test
+	@SuppressWarnings({ "unchecked" })
+	public void notFoundError() {
+		final String currencyCode = "XXX";
+
+		final HttpClientErrorException internalErrorException = new HttpClientErrorException(
+				HttpStatus.NOT_FOUND, "Not Found");
+
+		Mockito.when(this.restClient.get())
+				.thenReturn(this.requestHeadersUriSpec);
+		Mockito.when(this.requestHeadersUriSpec.uri(CurrencyEndpoints.CURRENCY.getValue(), currencyCode))
+				.thenReturn(this.requestHeadersSpec);
+		Mockito.when(this.requestHeadersSpec.retrieve()).thenThrow(internalErrorException);
+
+		Assertions.assertThrows(NoSuchElementException.class, () -> {
+			this.currencyClient.getCurrencyByCode(currencyCode);
+		});
+	}
+
+	/**
+	 * Remote internal server error.
+	 */
+	@Test
+	@SuppressWarnings({ "unchecked" })
+	public void remoteInternalServerError() {
+		final String currencyCode = "XXX";
+
+		final HttpServerErrorException internalErrorException = new HttpServerErrorException(
+				HttpStatus.INTERNAL_SERVER_ERROR, "Simulated Internal Server Error");
+
+		Mockito.when(this.restClient.get())
+				.thenReturn(this.requestHeadersUriSpec);
+		Mockito.when(this.requestHeadersUriSpec.uri(CurrencyEndpoints.CURRENCY.getValue(), currencyCode))
+				.thenReturn(this.requestHeadersSpec);
+		Mockito.when(this.requestHeadersSpec.retrieve()).thenThrow(internalErrorException);
+
+		Assertions.assertThrows(RemoteSystemAccessException.class, () -> {
+			this.currencyClient.getCurrencyByCode(currencyCode);
 		});
 	}
 
